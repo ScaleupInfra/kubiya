7dc66765049d35deb6195e0abd8c25c06ab8a54f
adityamurali155 <adityamurali2@gmail.com>
2023-12-09 18:11:25 +0530

    fixed changes

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-resource "aws_instance" "demo-instance" {
+resource "aws_instance" "env0-instance" {
      ami = "ami-0fc5d935ebf8bc3bc"
      instance_type = "t2.medium"
      count = 5
      tags = {
-       Name = "demo-instance-${count.index}"
+       Name = "env0-instance-${count.index}"
      }
 }
-resource "aws_s3_bucket" "demo-bucket" {
-  bucket = "demo-122312-test-bucket"
+resource "aws_s3_bucket" "env0-bucket" {
+  bucket = "env0-122312-test-bucket"
   tags = {
-    Name        = "Demo test bucket"
+    Name        = "env0 test bucket"
     Environment = "test"
   }
 }
