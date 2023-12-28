508e3c25ed17809dc444db7276f25591d1395f9d
shan0809 <shantanu_das90@outlook.com>
2023-12-28 14:17:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-resource "aws_instance" "env0-instance" {
+resource "aws_instance" "kubiya-instance" {
      ami = "ami-0fc5d935ebf8bc3bc"
      instance_type = "t2.medium"
      count = 5
      tags = {
-       Name = "env0-instance-${count.index}"
+       Name = "kubiya-instance-${count.index}"
      }
 }
-resource "aws_s3_bucket" "env0-bucket" {
-  bucket = "env0-122312-test-bucket"
+resource "aws_s3_bucket" "kubiya-bucket" {
+  bucket = "kubiya-122312-test-bucket"
   tags = {
-    Name        = "env0 test bucket"
+    Name        = "kubiya test bucket"
     Environment = "test"
   }
 }
